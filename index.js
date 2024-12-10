dc504c77be6fa710978df12723d8fb7459f0530c
Vaishu100000 <vsvaishu2002@gmail.com>
2024-12-10 09:26:05 +0530

    code written

---
+++ index.js
@@ -0,0 +1,22 @@
+function checkPrime(num) {
+    if (num <= 1) {
+        return false; 
+    }
+    if (num === 2) {
+        return true; 
+    }
+    if (num % 2 === 0) {
+        return false; 
+    }
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+ console.log(checkPrime(7));  
+ console.log(checkPrime(10)); 
+ console.log(checkPrime(13)); 
+
+
